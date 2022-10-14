d5aec569457e6d803070ab52e1263939fc26bfb9
prakriti583 <prakritivashishtha19@gmail.com>
2022-10-14 19:24:25 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class  extends StatefulWidget {
+  const ({Key? key}) : super(key: key);
+
+  @override
+  _State createState() => _State();
+}
+
+class _State extends State<> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
