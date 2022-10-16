be95710f084c91a66a471ec42845a8e28ee6fa32
prakriti583 <prakritivashishtha19@gmail.com>
2022-10-16 11:45:09 +0530

    Overflow error cleared

--- lib/Screens/RegisterNgo.dart
+++ lib/Screens/RegisterNgo.dart
@@ -89,163 +89,165 @@ class _RegisterNgoState extends State<RegisterNgo> {
                         EdgeInsets.symmetric(vertical: 20.0, horizontal: 20.0),
                         child: Form(
                             key: _formKey,
-                            child: Column(children: <Widget>[
-                              SizedBox(
-                                height: 60,
-                              ),
-                              TextFormField(
-                                decoration: InputDecoration(
-                                  hintText: 'Email',
-                                  hintStyle: TextStyle(
-                                      fontSize: 23,
-                                      color:
-                                      Color.fromARGB(0xFF, 0x98, 0x31, 0x6A),
-                                      fontWeight: FontWeight.w400),
+                            child: SingleChildScrollView(
+                              child: Column(children: <Widget>[
+                                SizedBox(
+                                  height: 60,
                                 ),
-                                validator: (val) =>
-                                val!.isEmpty ? 'Please enter email' : null,
-                                onChanged: (val) {
-                                  setState(() => email = val);
-                                },
-                              ),
-                              SizedBox(height: 25.0),
-                              TextFormField(
-                                decoration: InputDecoration(
-                                  hintText: 'Password',
-                                  hintStyle: TextStyle(
-                                      fontSize: 23,
-                                      color:
-                                      Color.fromARGB(0xFF, 0x98, 0x31, 0x6A),
-                                      fontWeight: FontWeight.w400),
+                                TextFormField(
+                                  decoration: InputDecoration(
+                                    hintText: 'Email',
+                                    hintStyle: TextStyle(
+                                        fontSize: 23,
+                                        color:
+                                        Color.fromARGB(0xFF, 0x98, 0x31, 0x6A),
+                                        fontWeight: FontWeight.w400),
+                                  ),
+                                  validator: (val) =>
+                                  val!.isEmpty ? 'Please enter email' : null,
+                                  onChanged: (val) {
+                                    setState(() => email = val);
+                                  },
                                 ),
-                                obscureText: true,
-                                validator: (val) => val!.length < 6
-                                    ? 'Enter a password of 6 or more characters'
-                                    : null,
-                                onChanged: (val) {
-                                  setState(() => password = val);
-                                },
-                              ),
+                                SizedBox(height: 25.0),
+                                TextFormField(
+                                  decoration: InputDecoration(
+                                    hintText: 'Password',
+                                    hintStyle: TextStyle(
+                                        fontSize: 23,
+                                        color:
+                                        Color.fromARGB(0xFF, 0x98, 0x31, 0x6A),
+                                        fontWeight: FontWeight.w400),
+                                  ),
+                                  obscureText: true,
+                                  validator: (val) => val!.length < 6
+                                      ? 'Enter a password of 6 or more characters'
+                                      : null,
+                                  onChanged: (val) {
+                                    setState(() => password = val);
+                                  },
+                                ),
 
-                              SizedBox(height: 20.0),
-                              _image != null
-                                  ? ClipRRect(
-                                borderRadius: BorderRadius.circular(50.0),
-                                child: Image.file(_image!,
-                                    width: 180,
-                                    height: 180,
-                                    fit: BoxFit.cover),
-                              )
-                                  : ClipRRect(
+                                SizedBox(height: 20.0),
+                                _image != null
+                                    ? ClipRRect(
                                   borderRadius: BorderRadius.circular(50.0),
-                                  child: FlutterLogo(size: 1)),
-                              Padding(
-                                padding: const EdgeInsets.fromLTRB(10, 0, 10, 20),
-                                child: SizedBox(
-                                  child: DecoratedBox(
-                                    decoration:
-                                    const BoxDecoration(color: Colors.white70),
-                                    child: Row(
-                                      children: [
-                                        FloatingActionButton(
-                                          onPressed: () {
-                                            getImage();
-                                          },
-                                          backgroundColor:
+                                  child: Image.file(_image!,
+                                      width: 180,
+                                      height: 180,
+                                      fit: BoxFit.cover),
+                                )
+                                    : ClipRRect(
+                                    borderRadius: BorderRadius.circular(50.0),
+                                    child: FlutterLogo(size: 1)),
+                                Padding(
+                                  padding: const EdgeInsets.fromLTRB(10, 0, 10, 20),
+                                  child: SizedBox(
+                                    child: DecoratedBox(
+                                      decoration:
+                                      const BoxDecoration(color: Colors.white70),
+                                      child: Row(
+                                        children: [
+                                          FloatingActionButton(
+                                            onPressed: () {
+                                              getImage();
+                                            },
+                                            backgroundColor:
  Color.fromARGB(
-                                      0x44, 0x98, 0x31, 0x6A),
-                                          child: Icon(Icons.add_a_photo_outlined),
+                                        0x44, 0x98, 0x31, 0x6A),
+                                            child: Icon(Icons.add_a_photo_outlined),
+                                          ),
+                                          Text(
+                                            '  Add Government Verified\n              Certificate',
+                                            style: TextStyle(
+                                                color: Color.fromARGB(
+                                                    0xFF, 0x98, 0x31, 0x6A),
+                                                fontSize: 18,
+                                                fontWeight: FontWeight.w500),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                SizedBox(height: 65,),
+                                ElevatedButton(
+                                    style: ButtonStyle(
+                                        shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                                            borderRadius: BorderRadius.circular(11)
+                                        )),
+                                        backgroundColor: MaterialStateProperty.all(Color.fromARGB(0xFF, 0x98, 0x31, 0x6A))
+                                    ),
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Text(
+                                        'Register',
+                                        style: TextStyle(
+                                            fontFamily: 'Mochiy Pop P One',
+                                            color: Colors.white,
+                                            fontSize: 27),
+                                      ),
+                                    ),
+                                    onPressed: () async {
+                                      if (_formKey.currentState!.validate()) {
+                                        dynamic result = await _auth
+                                            .registerWithEmailAndPassword(
+                                          email,
+                                          password,
+                                        );
+                                        if (result == null) { print("result1$email");
+                                          setState(() {
+                                            error = 'please supply a valid email';
+                                            loading = false;
+                                          });
+                                        } else {
+                                          Navigator.push(
+                                            context,
+                                            MaterialPageRoute(
+                                                builder: (context) =>Donate(lat:0,long:0)),
+                                          );
+                                          await DatabaseNGO(uid: user!.uid)
+                                              .updateUserData(
+                                            email,user!.uid,
+                                          );
+                                          }
+                                          Navigator.push(
+                                            context,
+                                            MaterialPageRoute(
+                                                builder: (context) => Donate(lat:0,long:0))
+                                          );
+                                        }
+
+                                    }),
+
+                                Row(
+                                  children: [
+                                    SizedBox(
+                                      width: 23,
+                                    ),
+                                    Text("Already have an account?",
+                                        style: TextStyle(fontSize: 17,
+                                            color: Color.fromARGB(
+                                                0x77, 0x98, 0x31, 0x6A),
+                                            fontWeight: FontWeight.w500)),
+                                    TextButton(
+                                        onPressed: () => Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                              builder: (context) => LoginNgo()),
                                         ),
-                                        Text(
-                                          '  Add Government Verified\n              Certificate',
-                                          style: TextStyle(
+                                        child: Text(
+                                          "Sign In",
+                                          style: TextStyle(fontSize: 17,
                                               color: Color.fromARGB(
                                                   0xFF, 0x98, 0x31, 0x6A),
-                                              fontSize: 18,
                                               fontWeight: FontWeight.w500),
-                                        ),
-                                      ],
-                                    ),
-                                  ),
+                                        ))
+                                  ],
                                 ),
-                              ),
-                              SizedBox(height: 65,),
-                              ElevatedButton(
-                                  style: ButtonStyle(
-                                      shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                                          borderRadius: BorderRadius.circular(11)
-                                      )),
-                                      backgroundColor: MaterialStateProperty.all(Color.fromARGB(0xFF, 0x98, 0x31, 0x6A))
-                                  ),
-                                  child: Padding(
-                                    padding: const EdgeInsets.all(8.0),
-                                    child: Text(
-                                      'Register',
-                                      style: TextStyle(
-                                          fontFamily: 'Mochiy Pop P One',
-                                          color: Colors.white,
-                                          fontSize: 27),
-                                    ),
-                                  ),
-                                  onPressed: () async {
-                                    if (_formKey.currentState!.validate()) {
-                                      dynamic result = await _auth
-                                          .registerWithEmailAndPassword(
-                                        email,
-                                        password,
-                                      );
-                                      if (result == null) { print("result1$email");
-                                        setState(() {
-                                          error = 'please supply a valid email';
-                                          loading = false;
-                                        });
-                                      } else {
-                                        Navigator.push(
-                                          context,
-                                          MaterialPageRoute(
-                                              builder: (context) =>Donate(lat:0,long:0)),
-                                        );
-                                        await DatabaseNGO(uid: user!.uid)
-                                            .updateUserData(
-                                          email,user!.uid,
-                                        );
-                                        }
-                                        Navigator.push(
-                                          context,
-                                          MaterialPageRoute(
-                                              builder: (context) => Donate(lat:0,long:0))
-                                        );
-                                      }
 
-                                  }),
-
-                              Row(
-                                children: [
-                                  SizedBox(
-                                    width: 21,
-                                  ),
-                                  Text("Already have an account?",
-                                      style: TextStyle(fontSize: 16.5,
-                                          color: Color.fromARGB(
-                                              0x77, 0x98, 0x31, 0x6A),
-                                          fontWeight: FontWeight.w500)),
-                                  TextButton(
-                                      onPressed: () => Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                            builder: (context) => LoginNgo()),
-                                      ),
-                                      child: Text(
-                                        "Sign In",
-                                        style: TextStyle(fontSize: 16.5,
-                                            color: Color.fromARGB(
-                                                0xFF, 0x98, 0x31, 0x6A),
-                                            fontWeight: FontWeight.w500),
-                                      ))
-                                ],
-                              ),
-
-                            ]))),
+                              ]),
+                            ))),
                  ),
                 ],
               )),
